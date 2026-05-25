b63f3463b6b4843e6b3047acbe01e666607bf6e3
Vlad <vladislavbelaev1@gmail.com>
2026-05-25 20:29:41 +0200

    fix: use one default welcome vault

--- meridian/src/renderer/src/utils/defaultVault.ts
+++ meridian/src/renderer/src/utils/defaultVault.ts
@@ -1,10 +1,3 @@
-export type PlatformName = string | undefined
-
-export function isMacPlatform(platform: PlatformName = globalThis.navigator?.platform): boolean {
-  return /Mac|iPhone|iPad|iPod/i.test(platform ?? '')
-}
-
-export function getWelcomeVaultPath(homeDir: string, platform?: PlatformName): string {
-  const vaultName = isMacPlatform(platform) ? 'Meridian Welcome' : 'Meridian Welcome (Windows)'
-  return `${homeDir}/Documents/${vaultName}`
+export function getWelcomeVaultPath(homeDir: string): string {
+  return `${homeDir}/Documents/Meridian Welcome`
 }
